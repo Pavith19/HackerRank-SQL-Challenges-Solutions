d5591f79868ab7434df055c980747d9a2f1a0b1e
Pavith Dinusara Bambaravanage <160596797+Pavith19@users.noreply.github.com>
2024-07-21 10:28:50 +0530

    Update Ollivander's Inventory.sql

--- Basic Join/Ollivander's Inventory.sql
+++ Basic Join/Ollivander's Inventory.sql
@@ -5,6 +5,11 @@ Hermione decides the best way to choose is by determining the minimum number of 
 Write a query to print the id, age, coins_needed, and power of the wands that Ron's interested in, sorted in order of descending power. 
 If more than one wand has same power, sort the result in order of descending age. */
 
+-- 
+-- Author: Pavith Bambaravanage
+-- URL: https://github.com/Pavith19
+-- 
+
 SELECT w.id, p.age, w.coins_needed, w.power
 FROM Wands w 
 INNER JOIN Wands_Property p ON w.code = p.code
