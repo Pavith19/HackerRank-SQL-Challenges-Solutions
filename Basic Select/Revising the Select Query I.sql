98e44e36679c29b27d1d3296c4b1e36c8d75e944
Pavith Dinusara Bambaravanage <160596797+Pavith19@users.noreply.github.com>
2024-07-21 10:30:38 +0530

    Update Revising the Select Query I.sql

--- Basic Select/Revising the Select Query I.sql
+++ Basic Select/Revising the Select Query I.sql
@@ -12,6 +12,11 @@
 
 Query all columns for all American cities in CITY with populations larger than 100000. The CountryCode for America is USA. */
 
+-- 
+-- Author: Pavith Bambaravanage
+-- URL: https://github.com/Pavith19
+-- 
+
 SELECT * FROM CITY 
   WHERE COUNTRYCODE = 'USA' 
     AND POPULATION > 100000;
