7de651fe36af1183acde9f16cfded2baa3d4b3db
Pavith Dinusara Bambaravanage <160596797+Pavith19@users.noreply.github.com>
2024-07-21 09:25:04 +0530

    Update Occupations.sql

--- Advanced Select/Occupations.sql
+++ Advanced Select/Occupations.sql
@@ -1,6 +1,11 @@
 /* Pivot the Occupation column in OCCUPATIONS so that each Name is sorted alphabetically and displayed underneath its corresponding Occupation. 
 The output column headers should be Doctor, Professor, Singer, and Actor, respectively. */
 
+-- 
+-- Author: Pavith Bambaravanage
+-- URL: https://github.com/Pavith19
+-- 
+
 SET @r1=0, @r2=0, @r3 =0, @r4=0;
 SELECT MIN(Doctor), MIN(Professor), MIN(Singer), MIN(Actor) FROM
 (SELECT CASE Occupation WHEN 'Doctor' THEN @r1:=@r1+1
