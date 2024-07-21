cce4cc3b293a12fd872055a45d95bc4eb2e5ea82
Pavith Dinusara Bambaravanage <160596797+Pavith19@users.noreply.github.com>
2024-07-21 10:25:03 +0530

    Update Weather Observation Station 14.sql

--- Aggregation/Weather Observation Station 14.sql
+++ Aggregation/Weather Observation Station 14.sql
@@ -13,6 +13,11 @@
 Query the greatest value of the Northern Latitudes (LAT_N) from STATION that is less than 137.2345. 
 Truncate your answer to 4 decimal places. */
 
+-- 
+-- Author: Pavith Bambaravanage
+-- URL: https://github.com/Pavith19
+-- 
+
 SELECT TRUNCATE(MAX(LAT_N), 4)
 FROM STATION
 WHERE LAT_N < 137.2345;
