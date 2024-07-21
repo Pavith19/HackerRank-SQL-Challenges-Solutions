bed4d7250d0ca07be46215ceaf0df580dc829ba5
Pavith Dinusara Bambaravanage <160596797+Pavith19@users.noreply.github.com>
2024-07-21 10:29:47 +0530

    Update Employees Salaries.sql

--- Basic Select/Employees Salaries.sql
+++ Basic Select/Employees Salaries.sql
@@ -13,6 +13,11 @@ Write a query that prints a list of employee names (i.e.: the name attribute)
 for employees in Employee having a salary greater than $2,000 per month who have been employees for less than 10 months. 
 Sort your result by ascending employee_id. */
 
+-- 
+-- Author: Pavith Bambaravanage
+-- URL: https://github.com/Pavith19
+-- 
+
 SELECT name FROM Employee
 WHERE salary > 2000 AND months < 10
 ORDER BY employee_id ASC;
