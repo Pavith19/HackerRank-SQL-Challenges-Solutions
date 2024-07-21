843661781ad53956cad963cdeb01b683f88598e9
Pavith Dinusara Bambaravanage <160596797+Pavith19@users.noreply.github.com>
2024-07-21 10:29:57 +0530

    Update Higher Than 75 Marks.sql

--- Basic Select/Higher Than 75 Marks.sql
+++ Basic Select/Higher Than 75 Marks.sql
@@ -11,6 +11,11 @@
 uery the Name of any student in STUDENTS who scored higher than 75 Marks. Order your output by the last three characters of each name.
 If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID. */
 
+-- 
+-- Author: Pavith Bambaravanage
+-- URL: https://github.com/Pavith19
+-- 
+
 SELECT Name FROM STUDENTS 
 WHERE Marks > 75
 ORDER BY RIGHT(Name, 3), ID ASC;
