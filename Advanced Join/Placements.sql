bb9e696941c3f40b788fcc0190f4b86346408c02
Pavith Dinusara Bambaravanage <160596797+Pavith19@users.noreply.github.com>
2024-07-21 09:23:42 +0530

    Update Placements.sql

--- Advanced Join/Placements.sql
+++ Advanced Join/Placements.sql
@@ -3,6 +3,11 @@
 Names must be ordered by the salary amount offered to the best friends.
 It is guaranteed that no two students got same salary offer. */
 
+-- 
+-- Author: Pavith Bambaravanage
+-- URL: https://github.com/Pavith19
+-- 
+
 SELECT s.name FROM Friends f, Packages p, Packages pf, Students s
 WHERE f.id=p.id AND f.friend_id=pf.id AND pf.salary>p.salary AND f.id=s.id
 ORDER BY pf.salary;
