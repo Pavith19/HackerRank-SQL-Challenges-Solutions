11cf7dbe3b7efdd99750658a6302385643f85dcc
Pavith Dinusara Bambaravanage <160596797+Pavith19@users.noreply.github.com>
2024-07-21 10:32:48 +0530

    Update Weather Observation Station 5.sql

--- Basic Select/Weather Observation Station 5.sql
+++ Basic Select/Weather Observation Station 5.sql
@@ -13,6 +13,11 @@
 Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name).
 If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically. */
 
+-- 
+-- Author: Pavith Bambaravanage
+-- URL: https://github.com/Pavith19
+-- 
+
 SELECT CITY, LENGTH(CITY) FROM STATION
 ORDER BY LENGTH(CITY), CITY
 LIMIT 1;
