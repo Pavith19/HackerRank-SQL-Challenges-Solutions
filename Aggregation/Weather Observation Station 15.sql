1104a0610abb6d035ed59ac3939b7ad139d1952e
Pavith Dinusara Bambaravanage <160596797+Pavith19@users.noreply.github.com>
2024-07-21 10:25:17 +0530

    Update Weather Observation Station 15.sql

--- Aggregation/Weather Observation Station 15.sql
+++ Aggregation/Weather Observation Station 15.sql
@@ -13,6 +13,11 @@
 Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION that is less than 137.2345. 
 Round your answer to 4 decimal places. */
 
+-- 
+-- Author: Pavith Bambaravanage
+-- URL: https://github.com/Pavith19
+-- 
+
 SELECT ROUND(LONG_W, 4)
 FROM STATION
 WHERE LAT_N = (SELECT MAX(LAT_N) FROM Station WHERE LAT_N < 137.2345);
