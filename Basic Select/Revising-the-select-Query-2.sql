a888389d7bc21ae5bdb6d00328185a3352cc734b
Pavith Dinusara Bambaravanage <160596797+Pavith19@users.noreply.github.com>
2024-07-21 10:30:50 +0530

    Update Revising-the-select-Query-2.sql

--- Basic Select/Revising-the-select-Query-2.sql
+++ Basic Select/Revising-the-select-Query-2.sql
@@ -12,6 +12,11 @@
 
 Query the names of all American cities in CITY with populations larger than 120000. The CountryCode for America is USA. */
 
+-- 
+-- Author: Pavith Bambaravanage
+-- URL: https://github.com/Pavith19
+-- 
+
 SELECT NAME FROM CITY 
   WHERE COUNTRYCODE = 'USA' 
     AND POPULATION > 120000;
