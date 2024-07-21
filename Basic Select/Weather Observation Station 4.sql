54f658d4a43b4f39daa42974163e48fce8533ee8
Pavith Dinusara Bambaravanage <160596797+Pavith19@users.noreply.github.com>
2024-07-21 10:32:57 +0530

    Update Weather Observation Station 4.sql

--- Basic Select/Weather Observation Station 4.sql
+++ Basic Select/Weather Observation Station 4.sql
@@ -14,4 +14,9 @@ Let N be the number of CITY entries in STATION, and let N’ be the number of di
 query the value of N - N’ from STATION. In other words, 
 find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table. */
 
+-- 
+-- Author: Pavith Bambaravanage
+-- URL: https://github.com/Pavith19
+-- 
+
 SELECT COUNT(CITY) - COUNT(DISTINCT CITY) FROM STATION ;
