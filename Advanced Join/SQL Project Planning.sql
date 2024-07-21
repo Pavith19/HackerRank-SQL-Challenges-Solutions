07522af985b24e35d1ffcde973b84d2f02af5a4d
Pavith Dinusara Bambaravanage <160596797+Pavith19@users.noreply.github.com>
2024-07-21 09:23:57 +0530

    Update SQL Project Planning.sql

--- Advanced Join/SQL Project Planning.sql
+++ Advanced Join/SQL Project Planning.sql
@@ -2,6 +2,11 @@
 /* Write a query to output the start and end dates of projects listed by the number of days it took to complete the project in ascending order. 
 If there is more than one project that have the same number of completion days, then order by the start date of the project. */
 
+-- 
+-- Author: Pavith Bambaravanage
+-- URL: https://github.com/Pavith19
+-- 
+
 SELECT Start_Date, MIN(End_Date)
 FROM (SELECT Start_Date FROM Projects WHERE Start_Date NOT IN 
     (SELECT End_Date FROM Projects)) a,
