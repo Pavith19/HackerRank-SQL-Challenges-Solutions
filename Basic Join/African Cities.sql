a815b6d858a51f541d1fbc450a0073e14bbaf8ad
Pavith Dinusara Bambaravanage <160596797+Pavith19@users.noreply.github.com>
2024-07-21 10:27:43 +0530

    Update African Cities.sql

--- Basic Join/African Cities.sql
+++ Basic Join/African Cities.sql
@@ -1,5 +1,9 @@
-
 /* Given the CITY and COUNTRY tables, query the names of all cities where the CONTINENT is 'Africa'. */
+
+-- 
+-- Author: Pavith Bambaravanage
+-- URL: https://github.com/Pavith19
+-- 
 
 SELECT CITY.NAME
 FROM CITY
