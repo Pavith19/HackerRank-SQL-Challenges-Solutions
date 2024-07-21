9e61c60457701e0a379c4f2a1e6c6939281c38a8
Pavith Dinusara Bambaravanage <160596797+Pavith19@users.noreply.github.com>
2024-07-21 10:24:49 +0530

    Update Weather Observation Station 13.sql

--- Aggregation/Weather Observation Station 13.sql
+++ Aggregation/Weather Observation Station 13.sql
@@ -13,6 +13,11 @@
 Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than 38.7880 and less than 137.2345. 
 Truncate your answer to 4 decimal places. */
 
+-- 
+-- Author: Pavith Bambaravanage
+-- URL: https://github.com/Pavith19
+-- 
+
 SELECT TRUNCATE(SUM(LAT_N), 4)
 FROM STATION
 WHERE LAT_N > 38.7880 AND LAT_N < 137.2345;
