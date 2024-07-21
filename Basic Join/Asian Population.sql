8bad05b0bb63c25087300404f633c053c61d6b86
Pavith Dinusara Bambaravanage <160596797+Pavith19@users.noreply.github.com>
2024-07-21 10:27:59 +0530

    Update Asian Population.sql

--- Basic Join/Asian Population.sql
+++ Basic Join/Asian Population.sql
@@ -1,5 +1,9 @@
-
 /* Given the CITY and COUNTRY tables, query the sum of the populations of all cities where the CONTINENT is 'Asia'. */
+
+-- 
+-- Author: Pavith Bambaravanage
+-- URL: https://github.com/Pavith19
+-- 
 
 SELECT SUM(CITY.POPULATION)
 FROM CITY
