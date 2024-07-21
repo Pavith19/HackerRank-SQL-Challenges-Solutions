7d820397171987e85b1a8dce76bc00c5acff0618
Pavith Dinusara Bambaravanage <160596797+Pavith19@users.noreply.github.com>
2024-07-21 10:31:16 +0530

    Update Select By ID.sql

--- Basic Select/Select By ID.sql
+++ Basic Select/Select By ID.sql
@@ -12,4 +12,9 @@
 
 Query all columns for a city in CITY with the ID 1661. */
 
+-- 
+-- Author: Pavith Bambaravanage
+-- URL: https://github.com/Pavith19
+-- 
+
 SELECT * FROM CITY WHERE ID = 1661;
