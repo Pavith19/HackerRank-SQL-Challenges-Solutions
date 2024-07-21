08967b65f4761eab6ad62a9f6d01b2ea3a6dd39c
Pavith Dinusara Bambaravanage <160596797+Pavith19@users.noreply.github.com>
2024-07-21 10:26:41 +0530

    Update Weather Observation Station 20.sql

--- Aggregation/Weather Observation Station 20.sql
+++ Aggregation/Weather Observation Station 20.sql
@@ -13,6 +13,11 @@
 A median is defined as a number separating the higher half of a data set from the lower half. 
 Query the median of the Northern Latitudes (LAT_N) from STATION and round your answer to 4 decimal places. */
 
+-- 
+-- Author: Pavith Bambaravanage
+-- URL: https://github.com/Pavith19
+-- 
+
 SELECT ROUND(x.LAT_N,4) from STATION x, STATION y
 GROUP BY x.LAT_N
 HAVING SUM(SIGN(1-SIGN(y.LAT_N-x.LAT_N))) = (COUNT(*)+1)/2
