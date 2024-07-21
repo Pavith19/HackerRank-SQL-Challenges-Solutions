9ec26b60e6d87e930a857635477056a44801d5de
Pavith Dinusara Bambaravanage <160596797+Pavith19@users.noreply.github.com>
2024-07-21 10:27:11 +0530

    Update Draw the triangle 1.sql

--- Alternative Queries/Draw the triangle 1.sql
+++ Alternative Queries/Draw the triangle 1.sql
@@ -1,6 +1,11 @@
 
 /* Write a query to print the pattern P(20). */
 
+-- 
+-- Author: Pavith Bambaravanage
+-- URL: https://github.com/Pavith19
+-- 
+
 
 SET @TEMP:=21; 
 SELECT REPEAT('* ', @TEMP:= @TEMP - 1) 
