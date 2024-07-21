8b6478d1d320e7584ddcc174de2847d4b1afc2d1
Pavith Dinusara Bambaravanage <160596797+Pavith19@users.noreply.github.com>
2024-07-21 10:25:41 +0530

    Update Weather Observation Station 16.sql

--- Aggregation/Weather Observation Station 16.sql
+++ Aggregation/Weather Observation Station 16.sql
@@ -13,6 +13,11 @@
 Query the smallest Northern Latitude (LAT_N) from STATION that is greater than 38.7880. 
 Round your answer to 4 decimal places. */
 
+-- 
+-- Author: Pavith Bambaravanage
+-- URL: https://github.com/Pavith19
+-- 
+
 SELECT ROUND(MIN(LAT_N), 4)
 FROM STATION
 WHERE LAT_N > 38.7780;
