df494e4444f3194c9f6180b6e0dfbc34444b2ef4
Pavith Dinusara Bambaravanage <160596797+Pavith19@users.noreply.github.com>
2024-07-21 10:32:33 +0530

    Update Weather Observation Station 6.sql

--- Basic Select/Weather Observation Station 6.sql
+++ Basic Select/Weather Observation Station 6.sql
@@ -11,6 +11,11 @@
 
 Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates. */
 
+-- 
+-- Author: Pavith Bambaravanage
+-- URL: https://github.com/Pavith19
+-- 
+
 SELECT DISTINCT CITY 
 FROM STATION 
 WHERE LEFT(CITY, 1) IN ('a','e','i','o','u');
