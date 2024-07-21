b72f201da9228b1f535c5ef31b717cd733290c1c
Pavith Dinusara Bambaravanage <160596797+Pavith19@users.noreply.github.com>
2024-07-21 10:20:59 +0530

    Update Average Population.sql

--- Aggregation/Average Population.sql
+++ Aggregation/Average Population.sql
@@ -12,5 +12,10 @@
 
 Query the average population for all cities in CITY, rounded down to the nearest integer. */
 
+/* *
+* Author: Pavith Bambaravanage
+* URL: https://github.com/Pavith19
+* */
+
 SELECT ROUND(AVG(POPULATION))
 FROM CITY;
