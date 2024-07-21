fbaa011ae0c2468534ee8b212c504b1fe780de8f
Pavith Dinusara Bambaravanage <160596797+Pavith19@users.noreply.github.com>
2024-07-21 10:26:27 +0530

    Update Weather Observation Station 2.sql

--- Aggregation/Weather Observation Station 2.sql
+++ Aggregation/Weather Observation Station 2.sql
@@ -14,5 +14,10 @@ Query the following two values from the STATION table:
 1. The sum of all values in LAT_N rounded to a scale of 2 decimal places.
 2. The sum of all values in LONG_W rounded to a scale of 2 decimal places. */
 
+-- 
+-- Author: Pavith Bambaravanage
+-- URL: https://github.com/Pavith19
+-- 
+
 SELECT ROUND(SUM(LAT_N), 2), ROUND(SUM(LONG_W), 2)
 FROM STATION;
