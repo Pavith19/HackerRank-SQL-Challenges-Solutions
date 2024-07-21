68ec3c86d4b90e3a84c5aaede506ad7fe2c09960
Pavith Dinusara Bambaravanage <160596797+Pavith19@users.noreply.github.com>
2024-07-21 10:30:07 +0530

    Update Japanese Cities Attributes.sql.sql

--- Basic Select/Japanese Cities Attributes.sql.sql
+++ Basic Select/Japanese Cities Attributes.sql.sql
@@ -12,5 +12,10 @@
 
 Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN. */
 
+-- 
+-- Author: Pavith Bambaravanage
+-- URL: https://github.com/Pavith19
+-- 
+
 SELECT * FROM CITY 
   WHERE COUNTRYCODE = 'JPN';
